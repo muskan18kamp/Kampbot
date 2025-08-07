da9ff9f5989be96b77cde220de989afd3a5a565e
muskan18kamp <muskan@kamp.org.in>
2025-08-07 11:31:52 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -145,3 +145,11 @@ def serve_ui():
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
